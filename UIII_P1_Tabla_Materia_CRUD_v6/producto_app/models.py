c2733939a3cf4039d89bf45763e79a4caca5fc09
Paola-ramirez128 <a.22308051281284@cbtis128.edu.mx>
2024-11-20 08:47:26 -0600

    Add files via upload

---
+++ UIII_P1_Tabla_Materia_CRUD_v6/producto_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Producto(models.Model):
+    id_producto=models.CharField(primary_key=True,max_length=10)
+    precio=models.CharField(max_length=20)
+    diseno=models.CharField(max_length=20)
+    fecha_entrega=models.CharField(max_length=20)
+    cantidad=models.PositiveSmallIntegerField()
+    fecha_elaboracion=models.CharField(max_length=20)
+    tamano=models.CharField(max_length=20)
+    def __str__(self):
+        return self.diseno
---
+++ UIII_P1_Tabla_Materia_CRUD_v6/producto_app/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect
+from .models import Producto
+# Create your views here.
+def inicio_vista(request):
+    losproductos=Producto.objects.all()
+    return render(request,"gestionarProducto.html",{"misproductos":losproductos})
+
+def registrarProducto(request):
+    id_producto=request.POST["txtcodigo"]
+    precio=request.POST["txtprecio"]
+    diseno=request.POST["txtdiseno"]
+    fecha_entrega=request.POST["txtfecha_entrga"]
+    cantidad=request.POST["numcantidad"]
+    fecha_elaboracion=request.POST["txtfecha_elaboracion"]
+    tamano=request.POST["txttamano"]
+
+    guardarProducto=Producto.objects.create(
+        id_producto=id_producto,precio=precio,diseno=diseno,fecha_entrega=fecha_entrega,cantidad=cantidad,fecha_elaboracion=fecha_elaboracion,tamano=tamano
+    )#GUARDAR EL REGISTRO
+    return redirect("/")
+
+def seleccionarProducto(request,id_prodcuto):
+    producto=Producto.objects.get(id_prodcuto=id_prodcuto)
+    return render(request,"editarproducto.html",{"misproductos":producto})
+
+def editarProducto(request):
+    id_producto=request.POST["txtcodigo"]
+    precio=request.POST["txtprecio"]
+    diseno=request.POST["txtdiseno"]
+    fecha_entrega=request.POST["txtfecha_entrga"]
+    cantidad=request.POST["numcantidad"]
+    fecha_elaboracion=request.POST["txtfecha_elaboracion"]
+    tamano=request.POST["txttamano"]
+    producto=Producto.objects.get(id_producto=id_producto)
+    producto.precio=precio
+    producto.diseno=diseno
+    producto.fecha_entrega=fecha_entrega
+    producto.cantidad=cantidad
+    producto.fecha_elaboracion=fecha_elaboracion
+    producto.tamano=tamano
+    producto.save() #guarda registro actualizado
+    return redirect("/")
+
+def borrarProducto(request,id_producto):
+    producto=Producto.objects.get(id_producto=id_producto)
+    producto.delete() #borra el resgistro
+    return redirect("/")
